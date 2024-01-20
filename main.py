93a79425a7474f1a88eff36dc75027db3c054f85
Bex Jonathan <bexjonathan777@gmail.com>
2024-01-20 23:18:17 +0100

    big graph

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from table import Table
 if __name__ == "__main__":
 
     for _ in range(100):
+        row, colmun = (20, 20)
 
-        graph = Graph(10, 10)
+        graph = Graph(row, colmun)
 
         print(graph.show())
 
-        graph1 = Graph(10, 10)
+        graph1 = Graph(row, colmun)
         print(graph1.show())
 
         labyrinth = graph + graph1
